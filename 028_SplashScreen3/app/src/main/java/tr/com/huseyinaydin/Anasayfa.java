895ce1d23b0599f22b3052d9efa0f5b9744e8ac2
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-11-26 18:47:48 +0300

    Android Splash Screen ana ekrana kullanıcı dostu geçiş.
    Bu proje, kullanıcıya çekici bir ilk izlenim sunmak amacıyla tasarlanmış bir açılış ekranıdır. Dinamik animasyonlar ve görsel efektlerle uygulamanın yüklenme süreci daha akıcı hale getirilmiştir.

---
+++ 028_SplashScreen3/app/src/main/java/tr/com/huseyinaydin/Anasayfa.java
@@ -0,0 +1,23 @@
+package tr.com.huseyinaydin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Android
+ *
+ */
+
+public class Anasayfa extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
---
+++ 028_SplashScreen3/app/src/main/java/tr/com/huseyinaydin/MainActivity.java
@@ -0,0 +1,67 @@
+package tr.com.huseyinaydin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Android
+ *
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode, event);
+        System.out.println("Selamlar");
+        if (KeyEvent.KEYCODE_MENU == keyCode) {
+            Intent intent = new Intent(MainActivity.this, Anasayfa.class);
+            startActivity(intent);
+        }
+        if (KeyEvent.KEYCODE_BACK == keyCode) {
+            finish();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent(MainActivity.this, Anasayfa.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
+}
